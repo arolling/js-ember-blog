be0a394258ef226852f022241a5472e59ca27c72
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-03-30 13:53:03 -0700

    Display blog entries

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    savePost(){
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+        datePosted: this.get('datePosted')
+      };
+      this.sendAction('addPost', params);
+    }
+  }
 });
